033060465884324177438fe7d022095b94091d5d
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-04-13 23:11:16 +0300

    task-01

--- src/index.js
+++ src/index.js
@@ -1,30 +1,80 @@
-//import './css/styles.css';
+import './css/styles.css';
+import {fetchCountries} from './js/fetchCountries';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
 
-//const DEBOUNCE_DELAY = 300;
-const countryList = document.querySelector('.country-list')
-const URL =  `https://restcountries.com/v3.1/all?fields=name,capital,population,flags,languages`;
+const DEBOUNCE_DELAY = 300;
+const countryList = document.querySelector('.country-list');
+const cauntryInfo = document.querySelector('.country-info')
+const inputText = document.querySelector('input#search-box');
 
-fetch(URL)
-.then((response) => response.json())
-.then((data) => {
-    console.log("data", data);
+inputText.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(e) {
+    e.preventDefault();
+
+    const inputValue = e.target.value.trim();
+    if (!inputValue) {
+        resetMarkup(countryList);
+        resetMarkup(cauntryInfo);
+        return;
+      }
+
+
+    fetchCountries(inputValue)
+    .then((data) => {
+    //console.log("data", data);
+    if (data.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name'
+        );
+        return;
+        }
      insertContent(data);
-})
-.catch((error) => {
-    console.log("error", error);
-})
+    })
+    .catch((error) => {
+        resetMarkup(countryList);
+        resetMarkup(cauntryInfo);
+        Notiflix.Notify.failure('Oops, there is no country with that name')
+    })
 
-const createListItem = (item) => `<li>
-    <img src="${item.flags}" alt="${item.flags}">
-    <h2>${item.name}</h2>
-    <h1>${item.capital}</h1>
-    <p>${item.population}</p>
-    <p>${item.languages}</p>
-</li>`;
-
-const generateContant = (array) => array.reduce((acc, item) => acc + createListItem(item), '');
-
-const insertContent = (array) => {
-    const result = generateContant(array);
-    countryList.insertAdjacentHTML('beforeend', result);
 }
+
+function resetMarkup(el) {
+    el.innerHTML = '';
+  }
+
+  function countryListItem(item) {
+    return item.map(
+        ({ name, flags }) =>
+          `<li class="link">
+          <img src="${flags.png}" alt="${name.official}" width="60" height="40"> 
+          <p class = "list-name";>${name.official}</p>
+          </li>`
+      )
+      .join('');
+  };
+
+function countryInfoItem(item) {
+    return item.map(
+        ({ name, capital, population, flags, languages }) =>
+          ` <div class="cauntry-items">  
+          <img src="${flags.png}" alt="${name.official}" width="200" height="100">
+          <h1>${name.official}</h1>
+          <p>Capital: ${capital}</p>
+          <p>Population: ${population}</p>
+          <p>Languages: ${Object.values(languages)}</p></div>`
+      );
+}
+
+function insertContent(country) {
+    if (country.length === 1) {
+        resetMarkup(countryListItem);
+        cauntryInfo.innerHTML = countryInfoItem(country);
+    } else {
+        resetMarkup(countryInfoItem);
+      countryList.innerHTML = countryListItem(country);
+    }
+  };
+
+
